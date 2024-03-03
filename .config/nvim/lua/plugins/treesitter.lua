ff003bf1599be83eaf82344dce5e3cc6d88c30d1
Felipe Trost <felipe.trost@gmail.com>
2024-03-03 19:52:21 +0100

    chore: moved treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,162 +1,158 @@
--- Defer Treesitter setup
-vim.defer_fn(function()
-  require 'nvim-treesitter.configs'.setup({
-    ignore_install = {},
-    modules = {},
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "bash", "go" },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
-
-
-    highlight = {
-      enable = true,
-
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
-    },
-    textobjects = {
-      select = {
-        enable = true,
-
-        -- Automatically jump forward to textobj, similar to targets.vim
-        lookahead = true,
-
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-          ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-          ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-          ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
-
-          -- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
-          ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
-          ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
-          ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
-          ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
-
-          ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
-          ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
-
-          ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-          ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-
-          ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-          ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-
-          ["ac"] = { query = "@call.outer", desc = "Select outer part of a function call" },
-          ["ic"] = { query = "@call.inner", desc = "Select inner part of a function call" },
-
-          ["af"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
-          ["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>na"] = "@parameter.inner",           -- swap parameters/argument with next
-          ["<leader>n:"] = "@property.outer",            -- swap object property with next
-          ["<leader>nm"] = "@function.outer",            -- swap function with next
-        },
-        swap_previous = {
-          ["<leader>pa"] = "@parameter.inner",           -- swap parameters/argument with prev
-          ["<leader>p:"] = "@property.outer",            -- swap object property with prev
-          ["<leader>pm"] = "@function.outer",            -- swap function with previous
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true,         -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]f"] = { query = "@call.outer", desc = "Next function call start" },
-          ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
-          ["]c"] = { query = "@class.outer", desc = "Next class start" },
-          ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
-          ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
-
-          -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-          -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-          ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-          ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-        },
-        goto_next_end = {
-          ["]F"] = { query = "@call.outer", desc = "Next function call end" },
-          ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
-          ["]C"] = { query = "@class.outer", desc = "Next class end" },
-          ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
-          ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
-        },
-        goto_previous_start = {
-          ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
-          ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
-          ["[c"] = { query = "@class.outer", desc = "Prev class start" },
-          ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
-          ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
-        },
-        goto_previous_end = {
-          ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
-          ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
-          ["[C"] = { query = "@class.outer", desc = "Prev class end" },
-          ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
-          ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
-        },
-      },
-    },
-
-  })
-
-  -- Folds
-  function HighlightedFoldtext()
-    local foldstart = vim.v.foldstart
-    local line = vim.api.nvim_buf_get_lines(0, foldstart - 1, foldstart, false)[1]
-
-    local line_count = vim.v.foldend - vim.v.foldstart + 1
-    local help_size = math.floor(line_count / 10)
-    local icon = ""
-
-    local firstChar = 1
-    while firstChar <= #line do
-      if not string.find(line, " ", firstChar) then
-        break
-      end
-      firstChar = firstChar + 1
-    end
-
-    local suffix = ": " .. line_count .. " lines " .. string.rep("▋", help_size)
-
-    if firstChar < string.len(icon) then
-      return line .. suffix
-    end
-
-    return icon .. " " .. string.sub(line, string.len(icon)) .. suffix
-  end
-
-  _G.HighlightedFoldtext = HighlightedFoldtext
-
-  vim.opt.fillchars = { eob = "-", fold = " " }
-  vim.opt.foldtext = "v:lua.HighlightedFoldtext()"
-  vim.opt.foldlevel = 99
-  vim.opt.foldlevelstart = 99
-  vim.opt.foldenable = true
-  vim.opt.foldmethod = "expr"
-  vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-end, 0)
-
-return {
-  {   -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
+return
+{ -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
   },
-  { "nvim-treesitter/playground" }
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ignore_install = {},
+      modules = {},
+      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "bash", "go" },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+
+
+      highlight = {
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+            ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+
+            -- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
+            ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
+            ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
+            ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
+            ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
+
+            ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+            ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+            ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+            ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+            ["ac"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+            ["ic"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+            ["af"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+            ["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
+            ["<leader>n:"] = "@property.outer",  -- swap object property with next
+            ["<leader>nm"] = "@function.outer",  -- swap function with next
+          },
+          swap_previous = {
+            ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
+            ["<leader>p:"] = "@property.outer",  -- swap object property with prev
+            ["<leader>pm"] = "@function.outer",  -- swap function with previous
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+            ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+            ["]c"] = { query = "@class.outer", desc = "Next class start" },
+            ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+            ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+
+            -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+            -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+          },
+          goto_next_end = {
+            ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+            ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+            ["]C"] = { query = "@class.outer", desc = "Next class end" },
+            ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+            ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+          },
+          goto_previous_start = {
+            ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+            ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+            ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+            ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+            ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+          },
+          goto_previous_end = {
+            ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+            ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+            ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+            ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+            ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
+          },
+        },
+      },
+
+    })
+
+    -- Folds
+    function HighlightedFoldtext()
+      local foldstart = vim.v.foldstart
+      local line = vim.api.nvim_buf_get_lines(0, foldstart - 1, foldstart, false)[1]
+
+      local line_count = vim.v.foldend - vim.v.foldstart + 1
+      local help_size = math.floor(line_count / 10)
+      local icon = ""
+
+      local firstChar = 1
+      while firstChar <= #line do
+        if not string.find(line, " ", firstChar) then
+          break
+        end
+        firstChar = firstChar + 1
+      end
+
+      local suffix = ": " .. line_count .. " lines " .. string.rep("▋", help_size)
+
+      if firstChar < string.len(icon) then
+        return line .. suffix
+      end
+
+      return icon .. " " .. string.sub(line, string.len(icon)) .. suffix
+    end
+
+    _G.HighlightedFoldtext = HighlightedFoldtext
+
+    vim.opt.fillchars = { eob = "-", fold = " " }
+    vim.opt.foldtext = "v:lua.HighlightedFoldtext()"
+    vim.opt.foldlevel = 99
+    vim.opt.foldlevelstart = 99
+    vim.opt.foldenable = true
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  end
 }
